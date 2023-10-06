7c53092007b1006e82fa3117e398353e5e2a586c
viltersander <sander.vilter123@gmail.com>
2023-10-06 18:37:03 +0300

    first commit

---
+++ src/scenes/footer/index.tsx
@@ -0,0 +1,33 @@
+import Logo from "@/assets/Logo.png";
+
+
+const Footer = () => {
+  return (
+    <footer className="bg-primary-100 py-16">
+      <div className="justify-content mx-auto w-5/6 gap-16 md:flex">
+        <div className="mt-16 basis-1/2 md:mt-0">
+          <img alt="logo" src={Logo} />
+          <p className="my-5">
+            Lorem vitae ut augue auctor faucibus eget eget ut libero. Elementum
+            purus et arcu massa dictum condimentum. Augue scelerisque iaculis
+            orci ut habitant laoreet. Iaculis tristique.
+          </p>
+          <p>© Evogym All Rights Reserved.</p>
+        </div>
+        <div className="mt-16 basis-1/4 md:mt-0 flex flex-col">
+          <h4 className="font-bold">Links</h4>
+          <a href="/" className="my-5 hover:text-primary-300">Massa orci senectus</a>
+          <a href="/" className="hover:text-primary-300">Et gravida id et etiam</a>
+          <a href="/" className="my-5 hover:text-primary-300">Ullamcorper vivamus</a>
+        </div>
+        <div className="mt-16 basis-1/4 md:mt-0">
+          <h4 className="font-bold">Contact Us</h4>
+          <p className="my-5">Tempus metus mattis risus volutpat egestas.</p>
+          <p>(333)425-6825</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
